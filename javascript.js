2e32f9f0273b6419bc45bbf2749eea6012a588b9
Clarice <claricemayhilton@gmail.com>
2019-03-31 17:47:55 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -68,6 +68,7 @@ function handleOrientation(event) {
   // It center the positioning point to the center of the ball
   ball.style.top  = (maxX*x/180 - 10) + "px";
   ball.style.left = (maxY*y/180 - 10) + "px";
+  console.log((maxX*x/180 - 10) + "px");
 }
 
 
