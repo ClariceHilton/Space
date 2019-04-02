f568310d3f1d181162e693f73200dcccc77ae34d
Clarice <claricemayhilton@gmail.com>
2019-04-02 19:58:56 +0100

    trying to get canvas back

--- animations.js
+++ animations.js
@@ -1,4 +1,4 @@
-
+console.log("here");
 
 swiper.on('slideChange', function() {
 if (swiper.realIndex == 1) {
@@ -16,7 +16,10 @@ if (swiper.realIndex == 1) {
 
     var tool = new paper.Tool();
 
-
+    /*tool.onMouseDown = function(event) {
+      console.log("here");
+        myPath.add(event.point);
+    }*/
     function handleOrientation(event) {
 
       var p = event.absolute;
@@ -29,26 +32,22 @@ if (swiper.realIndex == 1) {
 
       // Because we don't want to have the device upside down
       // We constrain the x value to the range [-90,90]
-      if (x >  90) { x =  90};
-      if (x < -90) { x = -90};
+      //if (x >  90) { x =  90};
+      //if (x < -90) { x = -90};
 
       // To make computation easier we shift the range of
       // x and y to [0,180]
-      x += 90;
+      /*x += 90;
       y += 90;
 
       // 10 is half the size of the ball
       // It center the positioning point to the center of the ball
       /*ball.style.top  = (maxX*x/180 - 10) + "px";
       ball.style.left = (maxY*y/180 - 10) + "px";*/
-      myPath.add(y, z);
-
-      console.log('here x:' + x + " y: " + y + " z: " +)z;
+      myPath.add(x, y);
+      console.log('here: ' + x + " " + y);
     }
-    /*tool.onMouseDown = function(event) {
-      console.log(event.point);
-        myPath.add(event.point);
-    }*/
+
 
     window.addEventListener('deviceorientation', handleOrientation);
 
