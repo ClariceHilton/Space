16a632aa23abaf31359c069313dcebe72766d0e4
Clarice <claricemayhilton@gmail.com>
2019-04-03 11:17:46 +0100

    update

---
+++ orientation.js
@@ -0,0 +1,28 @@
+function handleOrientation(event) {
+
+  var p = event.absolute;
+  var x = event.beta;  // In degree in the range [-180,180]
+  var y = event.gamma; // In degree in the range [-90,90]
+  var z = event.alpha;
+  var webkit = event.webkitCompassHeading;
+  var accuracy = event.webkitCompassAccuracy;
+  var mag = event.magnetometer
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
+  // To make computation easier we shift the range of
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  /*ball.style.top  = (maxX*x/180 - 10) + "px";
+  ball.style.left = (maxY*y/180 - 10) + "px";*/
+globals.gx = x;
+globals.gy = y;
+  console.log('here: ' + x + " " + y);
+}
