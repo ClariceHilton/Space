09554921fe9d014dcbaa43427b84358bf7410bfe
Clarice <claricemayhilton@gmail.com>
2019-04-04 08:38:16 +0100

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -8,7 +8,7 @@ project.currentStyle = {
 };
 
 var ballPositions = [[255, 129], [610, 73], [486, 363],
-  [117, 459], [484, 726], [843, 306], [789, 615], [92, 798], [190, 1152], [400, 1428]];
+  [117, 459], [484, 726], [843, 306], [789, 615], [92, 798], [190, 1120], [400, 1400], [484, 950], [852, 802]];
 
 var handle_len_rate = 2.4;
 var circlePaths = [];
@@ -30,8 +30,8 @@ circlePaths.push(largeCircle);
 view.onFrame = function(event) {
     // Every frame, rotate the path by 3 degrees:
 
-    largeCircle.position.x = globals.gz*-5;
-    largeCircle.position.y = globals.gy*5;
+    largeCircle.position.x = globals.gy*5;
+    largeCircle.position.y = globals.gx*5;
     generateConnections(circlePaths);
 
 }
